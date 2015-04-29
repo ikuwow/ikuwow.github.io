d25398b5ad39dccee01268a1883b35340f9ddea1
Ikuo Degawa <degawa@ikuwow.com>
2015-04-29 17:36:07 +0900

    Fix navbar bug

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,19 +1,19 @@
 $(function(){
 
-$(".nav-hamburger-content").hide();
-$(".nav-cross").hide();
+$(".nav-hamburger-content").addClass("dn");
+$(".nav-cross").addClass("dn");
 
 $(".nav-hamburger").click(function(){
     $(".nav-hamburger-content").slideToggle("fast",function(){
-            $(".nav-hamburger").hide();
-            $(".nav-cross").show();
+            $(".nav-hamburger").addClass("dn");
+            $(".nav-cross").removeClass("dn");
     });
 });
 
 $(".nav-cross").click(function(){
     $(".nav-hamburger-content").slideToggle("fast",function(){
-            $(".nav-hamburger").show();
-            $(".nav-cross").hide();
+            $(".nav-hamburger").removeClass("dn");
+            $(".nav-cross").addClass("dn");
     });
 });
 
