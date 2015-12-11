2e2297c5cacbefdf5aedc8e36814d578eeca1c7f
Ikuo Degawa <degawa@ikuwow.com>
2015-12-11 22:32:30 +0900

    Remove heavy share buttons

--- js/common.js
+++ js/common.js
@@ -13,11 +13,11 @@ culc_age = function(){
 
     var current_time = Date.now();
     var distance = current_time - birthday;
-    
+
     current_age = distance/(365*24*60*60*1000);
     after_dot = Math.pow(10,10);
     current_age = Math.round(current_age*after_dot)/after_dot;
-    
+
     $(".age").html(current_age.toFixed(9));
 
     setTimeout(culc_age,100);
