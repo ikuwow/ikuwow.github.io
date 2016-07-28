251b3f7958f03bd406f1d5126181a9d6514589ec
Ikuo Degawa <degawa@ikuwow.com>
2016-07-28 21:36:09 +0900

    fix copyright

--- js/common.js
+++ js/common.js
@@ -1,21 +1,18 @@
 $(function(){
 
-/*
-$(".top-portrait").click(function(){
-    alert("Thank you for tapping me!");
-});
-*/
+'use strict';
 
 /* age calcuration */
 var birthday = new Date("1990-09-13T03:47:00").getTime();
 
+var culc_age;
 culc_age = function(){
 
     var current_time = Date.now();
     var distance = current_time - birthday;
 
-    current_age = distance/(365*24*60*60*1000);
-    after_dot = Math.pow(10,10);
+    var current_age = distance/(365*24*60*60*1000);
+    var after_dot = Math.pow(10,10);
     current_age = Math.round(current_age*after_dot)/after_dot;
 
     $(".age").html(current_age.toFixed(9));
@@ -25,7 +22,13 @@ culc_age = function(){
 
 culc_age();
 
+});
 
+$(function(){
 
+'use strict';
+
+var copyright_to = document.getElementById('copyright-to-year');
+copyright_to.innerHTML = (new Date()).getFullYear();
 
 });
