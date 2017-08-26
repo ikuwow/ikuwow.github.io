f74ec04932530f3484444b12213abd57518b9874
Ikuo Degawa <degawa@ikuwow.com>
2017-08-26 17:22:16 +0900

    Remove use of jquery in common.js

--- js/common.js
+++ js/common.js
@@ -1,12 +1,11 @@
-$(function(){
+(function(){
 
 'use strict';
 
-/* age calcuration */
+// age calcuration
 var birthday = new Date("1990-09-13T03:47:00").getTime();
 
-var culc_age;
-culc_age = function(){
+var culc_age = function(){
 
     var current_time = Date.now();
     var distance = current_time - birthday;
@@ -15,20 +14,20 @@ culc_age = function(){
     var after_dot = Math.pow(10,10);
     current_age = Math.round(current_age*after_dot)/after_dot;
 
-    $(".age").html(current_age.toFixed(9));
-
+    var ageField = document.querySelector('.age');
+    ageField.innerHTML = current_age.toFixed(9);
     setTimeout(culc_age,100);
 };
 
 culc_age();
 
-});
+})();
 
-$(function(){
+(function(){
 
 'use strict';
 
 var copyright_to = document.getElementById('copyright-to-year');
 copyright_to.innerHTML = (new Date()).getFullYear();
 
-});
+})();
