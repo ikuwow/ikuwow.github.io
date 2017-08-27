2b34a7481097993f58c7e5a1de1c5af1a7b30cc9
Ikuo Degawa <degawa@ikuwow.com>
2017-08-27 18:12:24 +0900

    remove overlay

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,29 +2,17 @@
 
 'use strict';
 
-var overlay = document.createElement("div");
-overlay.className = "overlay";
-document.getElementById("main").appendChild(overlay);
-
 var hamburger_content = document.querySelector('.nav-hamburger-content');
-hamburger_content.classList.add("dn");
+hamburger_content.classList.add('dn');
 
 // display menu
-$(".nav-hamburger").click(function(){
-    $('.overlay').toggle(); // オーバーレイ表示切替
-    $(".nav-hamburger-content").slideToggle("fast",function(){
-        $(".nav-hamburger").addClass("dn");
+$('.nav-hamburger').click(function(){
+    $('.nav-hamburger-content').slideToggle('fast', function(){
+        $('.nav-hamburger').addClass('dn');
     });
 });
 
-// close menu when overlay tapped
-$('.overlay').click(function(){
-	$(this).toggle(); // オーバーレイ非表示
-    $(".nav-hamburger-content").slideToggle("fast");
-});
-
-$(".nav-hamburger-content li").click(function(){
-    $('.overlay').toggle(); // オーバーレイ表示切替
+$(".nav-hamburger-content ul").click(function(){
     $('.nav-hamburger-content').toggle();
 });
 
