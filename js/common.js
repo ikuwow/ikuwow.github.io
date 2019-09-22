6f25fa6d200cfaff7354182c63c1e5fa10eaba85
Ikuo Degawa <degawa@ikuwow.com>
2019-09-23 00:39:44 +0900

    Remove current age

--- js/common.js
+++ js/common.js
@@ -2,31 +2,6 @@
 
 'use strict';
 
-// age calcuration
-var birthday = new Date("1990-09-13T03:47:00").getTime();
-
-var culc_age = function(){
-
-    var current_time = Date.now();
-    var distance = current_time - birthday;
-
-    var current_age = distance/(365*24*60*60*1000);
-    var after_dot = Math.pow(10,10);
-    current_age = Math.round(current_age*after_dot)/after_dot;
-
-    var ageField = document.querySelector('.age');
-    ageField.innerHTML = current_age.toFixed(9);
-    setTimeout(culc_age,100);
-};
-
-culc_age();
-
-})();
-
-(function(){
-
-'use strict';
-
 var copyright_to = document.getElementById('copyright-to-year');
 copyright_to.innerHTML = (new Date()).getFullYear();
 
